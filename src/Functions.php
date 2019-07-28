977493c04485f8738b57d0fc1f26853552d2443c
test <864328615@qq.com>
2019-07-28 12:43:08 +0800

    del Function.php

--- src/Functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace yunqi\helper;
-
-class Functions
-{
-    
-}
--- src/encryption/RSA.php
+++ src/encryption/RSA.php
@@ -1,7 +1,30 @@
 <?php
-namespace yunqi\helper;
+namespace yunqi\helper\encryption;
+use yunqi\helper\Functions;
 
+/**
+ * RSA加密解密
+ * @author destiny
+ */
 class RSA
 {
-    
+    /**
+     * 私钥分段加密
+     * @param [type] $data
+     * @param [type] $key
+     * @return void
+     * @author destiny
+     */
+    public static function Encrypt($str,$key)
+    {
+        $key = Functions::getKey($key);
+        $encrypt = '';
+        foreach (str_split($str, 117) as $chunk)
+        {
+            openssl_public_encrypt($chunk,$encryptData,$key);
+            $encrypt .= $encryptData;
+        }
+        openssl_free_key($key);
+        return $encrypt;
+    }
 }
